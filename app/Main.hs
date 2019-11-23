41151ac181616b90c2df19e3036eac3322255013
Hielke de Vries <hdevries@fastmail.com>
2019-11-23 21:16:24 +0100

    print all sudoku suggestions

--- app/Main.hs
+++ app/Main.hs
@@ -3,33 +3,23 @@ module Main where
 import Sudokus
 import qualified Data.Set as Set
 import qualified Data.List as List
+import qualified Data.List.Split as Split
 
 main :: IO ()
 main = do
-    print (validateSudoku validSudoku1)
-    print (validateSudoku invalidSudoku2)
-    print (fillSudoku [[1]])
+    print "aap"
+    print allPossibleSudokus
 
 
-
-
-
-
-
-
-    
-toSolveSudoku :: [[Int]]
-toSolveSudoku = [[0,2,0], [2,3,0], [0,1,2]]
-
-
+-- fill sudoku
 sudokuToSolve :: [[Int]]
 sudokuToSolve = [[0,2,0], [2,3,0], [0,1,2]]
 
 
 allPossibleSudokus :: [[[Int]]]
 allPossibleSudokus =
-    let allCombinations = combinations [1..3] 9
-    in map (\x -> Split.chunksOf 3 x) allCombinations
+    let allCombinations = allPossibleCombination [1..3] 9
+    in map (\x -> fillSudoku sudokuToSolve (Split.chunksOf 3 x) ) allCombinations
 
 
 fillSudoku :: [[Int]] -> [[Int]] -> [[Int]]
@@ -39,16 +29,13 @@ fillSudoku sudokuToSolve iteration =
         combined = zipWith sumIfZero flat1 flat2
     in Split.chunksOf 3 combined
 
-
-
-
 sumIfZero :: Int -> Int -> Int
 sumIfZero x y = 
     if x == 0
         then x + y
         else x
 
-combinations xs n = mapM (const xs) [1..n]
+allPossibleCombination xs n = mapM (const xs) [1..n]
 
 
 -- validate sudoku
@@ -63,8 +50,3 @@ foldBools x = foldr (&&) True x
 validateRow :: [Int] -> Bool
 validateRow [] = False
 validateRow x = (Set.fromList [1..3]) == (Set.fromList x)
-
-
-fillSudoku :: [[Int]] -> [[Int]]
-fillSudoku toSolve =
-    [ map (\x -> x * x) [1..10] ]
